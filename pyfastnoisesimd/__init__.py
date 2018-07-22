ca5b0c17821ed6b39299d3c1b913a9bca47737aa
Elliott Sales de Andrade <quantum.analyst@gmail.com>
2018-07-22 00:42:23 -0400

    Remove unused cpuinfo.py.

--- pyfastnoisesimd/__init__.py
+++ pyfastnoisesimd/__init__.py
@@ -20,6 +20,3 @@ from pyfastnoisesimd.helpers import (
 import pyfastnoisesimd.extension as _ext
 
 from pyfastnoisesimd.test_fns import test
-
-# from pyfastnoisesimd.cpuinfo import get_cpu_info
-# cpu_info = get_cpu_info()
